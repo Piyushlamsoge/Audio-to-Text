1dffb330085185822c29bd9c5bbc74736c33fdda
Piyush Lamsoge <piyushlamsoge20@gmail.com>
2023-06-12 18:32:40 +0530

    version_1

--- speech_to_text.py
+++ speech_to_text.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1Veu-PpZgSIhgZAiiRDavAq-6n1PTfZxv
 """
 
-!pip install streamlit
+
 
 import streamlit as st
 import speech_recognition as sr
